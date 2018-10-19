f30190d7fdb3df047bbbd81e43135200e314b439
Jhonnatthan <jhonnatthan@Jhonnatthans-MacBook-Pro.local>
2018-10-19 01:03:14 -0300

    [UPDATE] - Registro de Vendas (Opções de produtos)

--- src/pages/registrar-venda/registrar-venda.ts
+++ src/pages/registrar-venda/registrar-venda.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, AlertController, ModalController } from 'ionic-angular';
+import { PesquisaPage } from '../pesquisa/pesquisa';
 
 @Component({
     selector: 'page-registrar-venda',
@@ -7,23 +8,33 @@ import { IonicPage, NavController, NavParams, ActionSheetController } from 'ioni
 })
 export class RegistrarVendaPage {
 
-    constructor(public navCtrl: NavController, 
+    produtos: any = [];
+    pagamentos: any = [];
+    observacoes: any = [];
+
+    total: any = 0;
+
+    constructor(public navCtrl: NavController,
         public navParams: NavParams,
-        public actionSheetCtrl: ActionSheetController) {
+        public actionSheetCtrl: ActionSheetController,
+        public alertCtrl: AlertController,
+        public modalCtrl: ModalController) {
     }
 
     ionViewDidLoad() {
-        console.log('ionViewDidLoad RegistrarVendaPage');
     }
 
-    adicionarItem() {
+    adicionarItem = () => {
         this.actionSheetCtrl.create({
-            title: 'Adicionar item',
+            title: 'Adicionar a venda',
             enableBackdropDismiss: true,
             buttons: [
                 {
                     text: 'Produto',
-                    icon: 'basket'
+                    icon: 'basket',
+                    handler: () => {
+                        this.selecionaProduto('add');
+                    }
                 },
                 {
                     text: 'Pagamento',
@@ -41,4 +52,130 @@ export class RegistrarVendaPage {
         }).present();
     }
 
+    menuProduto = (index) => {
+        let produto = this.produtos[index];
+        this.actionSheetCtrl.create({
+            title: 'Menu do produto',
+            enableBackdropDismiss: true,
+            buttons: [
+                {
+                    text: 'Alterar quantidade',
+                    handler: () => {
+                        this.alterarQuantidade(produto);
+                    }
+                },
+                {
+                    text: 'Alterar produto',
+                    handler: () => {
+                        this.selecionaProduto('update', index);
+                    }
+                },
+                {
+                    text: 'Remover',
+                    role: 'destructive',
+                    handler: () => {
+                        this.removeProduto(produto);
+                    }
+                },
+                {
+                    text: 'Cancelar',
+                    role: 'cancel'
+                }
+            ]
+        }).present();
+    }
+
+     alterarQuantidade = async (produto) => {
+        let quantidade = await this.alertValor('Bolsa Stylus', 'Quantidade:', 'quantidade', 'tel');
+        produto.quantity = quantidade;
+        this.calcularValor();
+    }
+
+    selecionaProduto = (event, index?) => {
+        let produto = this.modalCtrl.create(PesquisaPage, {
+            title: 'Produtos',
+            data: [
+                {
+                    id: 1,
+                    name: 'Bolsa',
+                    quantity: 1,
+                    price: 0
+                },
+                {
+                    id: 2,
+                    name: 'Oculos',
+                    quantity: 1,
+                    price: 0
+                },
+                {
+                    id: 3,
+                    name: 'Relógio',
+                    quantity: 1,
+                    price: 0
+                },
+            ],
+            seachbar: true
+        });
+
+        produto.onDidDismiss(async data => {
+            let produto = data;
+
+            if (event == 'add') {
+                let preco = await this.alertValor(produto.name, 'Valor: ', 'valor', 'tel');
+                produto.price = preco;
+                this.produtos.push(produto);
+            } else if (event == 'update') {
+                let preco = await this.alertValor(produto.name, 'Valor: ', 'valor', 'tel');
+                produto.price = preco;
+                this.produtos[index] = produto;
+            }
+
+            this.calcularValor();
+        });
+
+        produto.present();
+    }
+    
+    removeProduto = (_produto) => {
+        this.produtos = this.produtos.filter(produto => produto.id != _produto.id);
+        this.calcularValor();
+    }
+
+    calcularValor = () => {
+        this.total = 0;
+        this.produtos.forEach(produto => {
+            this.total += (produto.price * produto.quantity);
+        });
+    }
+
+    alertValor = (titulo, message, inputName, inputType) => {
+        return new Promise((resolve, reject) => {
+            this.alertCtrl.create({
+                title: titulo,
+                message: message,
+                inputs: [
+                    {
+                        name: inputName,
+                        type: inputType,
+                        min: 1
+                    }
+                ],
+                buttons: [
+                    {
+                        text: 'Cancelar',
+                        handler: data => {
+                            resolve(false);
+                        }
+                    },
+                    {
+                        text: 'Salvar',
+                        handler: data => {
+                            resolve(data[inputName]);
+                        }
+                    }
+                ]
+            }).present();
+        });
+
+    }
 }
